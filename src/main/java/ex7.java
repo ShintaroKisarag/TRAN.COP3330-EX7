20dd1f7824f39db599aa20486f44b73239b54b65
Phi-Hung Tran <Phi-Hung.phtran2002@gmail.com>
2021-09-12 23:14:18 -0700

    Initial commit

---
+++ src/main/java/ex7.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 7 Solution
+ *  Copyright 2021 Phi-Hung Tran
+ */
+
+import java.util.Scanner;
+
+public class ex7 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the length of the room in feet?");
+        double length = input.nextDouble(); //stores length of room
+        System.out.println("What is the width of the room in feet?");
+        double width=input.nextDouble();    //stores width of room
+        System.out.println("You entered dimension of "+length+ " by "+width+ " feet ");
+
+        double area = length * width;   //formula of area
+        final double conversionFactor=area*0.09290304;
+
+        System.out.println("The area is ");
+        System.out.println(area+" square feet");
+        System.out.println(conversionFactor+ " square meters");
+    }
+}
